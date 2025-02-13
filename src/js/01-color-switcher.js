f7ee14a4bdcd00eb1f773da1afe847f6e783f213
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2025-02-13 23:15:54 +0200

    task_01_done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
 
+let intervalID = null;
+
+refs.startBtn.addEventListener('click', onStartRandomColor);
+refs.stopBtn.addEventListener('click', onStopRandomColor);
+
+function onStartRandomColor() {
+    intervalID = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+    }, 1000);
+    changeButtonStatus(true, false)
+}
+
+function onStopRandomColor() {
+    clearInterval(intervalID);
+    changeButtonStatus(false, true)
+}
+
+function changeButtonStatus(start, stop) {
+    refs.startBtn.disabled = start
+    refs.stopBtn.disabled = stop
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
