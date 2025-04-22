a82a9b776fbe17c4a392cfce6bace01ea060a6f8
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2025-04-22 12:07:16 +0300

    start_03_task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,19 @@
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onStart)
+
+function onStart(e) {
+  e.preventDefault()
+  const delay = Number(e.currentTarget.elements.delay.value)
+  const step = Number(e.currentTarget.elements.step.value)
+  const amount = Number(e.currentTarget.elements.amount.value)
+
+  listOfDelay(delay, step, amount)
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
